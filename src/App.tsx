c9cfedbd259fd5d09f03ea21733b6aeeadb3832f
JCWoodworker <jc@rilocalwoodworks.com>
2024-02-04 09:47:37 -0500

    setting up basic loggen in logic

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,21 @@
+import { useState } from "react"
+import { Routes, Route } from "react-router-dom"
 
+import Home from "./components/Home"
+import UserHome from "./components/UserHome"
 function App() {
-	return <>APP</>
+	const [loggedIn, setLoggedIn] = useState(true)
+	return (
+		<>
+			<Routes>
+				{loggedIn ? (
+					<Route path="/" element={<UserHome />}></Route>
+				) : (
+					<Route path="/" element={<Home />}></Route>
+				)}
+			</Routes>
+		</>
+	)
 }
 
 export default App
